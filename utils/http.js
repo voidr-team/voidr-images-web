17c5832a8b5154ff0451eedda2a59097416e9ddb
buchalla <vs_buchalla@hotmail.com>
2023-08-30 15:12:06 -0300

    fix: http

--- utils/http.js
+++ utils/http.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const COLABY_API_URL = process.env.NEXT_PUBLIC_COLABY_API_URL
+const VOIDR_API_URL = process.env.NEXT_PUBLIC_VOIDR_API_URL
 
 const http = axios.create({
-  baseURL: `${COLABY_API_URL}`,
+  baseURL: `${VOIDR_API_URL}`,
 })
 
 export default http
