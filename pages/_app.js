883fb4eed6e0665bed45da3e80128360ca8067c0
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-08-31 15:19:37 -0300

    style: :lipstick: creating ui for login

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,142 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import '@fontsource/space-grotesk'
 import '@fontsource/inter'
 
+import { extendTheme, CssVarsProvider } from '@mui/joy/styles'
+import { CssBaseline } from '@mui/joy'
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          50: '#F9F8FC',
+          100: '#E8E5F8',
+          200: '#D2CCF3',
+          300: '#827BA2',
+          400: '#5D5879',
+          500: '#3B3750',
+          600: '#272436',
+          700: '#1E272E',
+          800: undefined,
+          900: undefined,
+        },
+        neutral: {
+          50: '#FAFAFA',
+          100: '#F5F5F5',
+          200: '#EEEEEE',
+          300: '#E1E1E1',
+          400: '#CACACA',
+          500: '#8E8E8E',
+          600: '#4B4B4B',
+          700: '#1F1F1F',
+          800: undefined,
+          900: undefined,
+        },
+        danger: {
+          50: '#FFFBFB',
+          100: '#FEF2F2',
+          200: '#FFEBEE',
+          300: '#FFCCD2',
+          400: '#F49898',
+          500: '#EB6F70',
+          600: '#F64C4C',
+          700: '#EC2D30',
+          800: undefined,
+          900: undefined,
+        },
+        warning: {
+          50: '#FFFDFA',
+          100: '#FFF9EE',
+          200: '#FFF7E1',
+          300: '#FFEAB3',
+          400: '#FFDD82',
+          500: '#FFC62B',
+          600: '#FFAD0D',
+          700: '#FE9B0E',
+          800: undefined,
+          900: undefined,
+        },
+        success: {
+          50: '#FBFEFC',
+          100: '#F2FAF6',
+          200: '#E5F5EC',
+          300: '#C0E5D1',
+          400: '#97D4B4',
+          500: '#6BC497',
+          600: '#47B881',
+          700: '#0C9D61',
+          800: undefined,
+          900: undefined,
+        },
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          50: '#F9F8FC',
+          100: '#E8E5F8',
+          200: '#D2CCF3',
+          300: '#827BA2',
+          400: '#5D5879',
+          500: '#3B3750',
+          600: '#272436',
+          700: '#1E272E',
+          800: undefined,
+          900: undefined,
+        },
+        neutral: {
+          50: '#FAFAFA',
+          100: '#F5F5F5',
+          200: '#EEEEEE',
+          300: '#E1E1E1',
+          400: '#CACACA',
+          500: '#8E8E8E',
+          600: '#4B4B4B',
+          700: '#1F1F1F',
+          800: undefined,
+          900: undefined,
+        },
+        danger: {
+          50: '#FFFBFB',
+          100: '#FEF2F2',
+          200: '#FFEBEE',
+          300: '#FFCCD2',
+          400: '#F49898',
+          500: '#EB6F70',
+          600: '#F64C4C',
+          700: '#EC2D30',
+          800: undefined,
+          900: undefined,
+        },
+        warning: {
+          50: '#FFFDFA',
+          100: '#FFF9EE',
+          200: '#FFF7E1',
+          300: '#FFEAB3',
+          400: '#FFDD82',
+          500: '#FFC62B',
+          600: '#FFAD0D',
+          700: '#FE9B0E',
+          800: undefined,
+          900: undefined,
+        },
+        success: {
+          50: '#FBFEFC',
+          100: '#F2FAF6',
+          200: '#E5F5EC',
+          300: '#C0E5D1',
+          400: '#97D4B4',
+          500: '#6BC497',
+          600: '#47B881',
+          700: '#0C9D61',
+          800: undefined,
+          900: undefined,
+        },
+      },
+    },
+  },
+})
+
 const VoidrApp = ({ Component, pageProps, ...props }) => {
   const currentRoute = props.router.route
   const publicRoutes = ['/login', '/signin', '/logout']
@@ -22,7 +158,10 @@ const VoidrApp = ({ Component, pageProps, ...props }) => {
   return (
     <Auth0Wrapper>
       <AuthProvider>
-        <LoggedLayout currentPage={currentRoute}>{BaseComp}</LoggedLayout>
+        <CssVarsProvider theme={theme}>
+          <CssBaseline />
+          <LoggedLayout currentPage={currentRoute}>{BaseComp}</LoggedLayout>
+        </CssVarsProvider>
         <ToastContainer />
       </AuthProvider>
     </Auth0Wrapper>
--- pages/login/index.js
+++ pages/login/index.js
@@ -5,20 +5,55 @@ import loadAllTranslations from '@/utils/i18n/loadAllTranslations'
 import { useTranslation } from 'next-i18next'
 import { useAuth0 } from '@auth0/auth0-react'
 import styles from './Login.module.scss'
+import Logo from '@/public/images/logo.svg'
+import Pattern from '@/public/images/pattern.svg'
+import Image from 'next/image'
+
+import { Stack, Typography, Button } from '@mui/joy'
 
 function LoginPage() {
   const { t } = useTranslation()
   const { loginWithRedirect } = useAuth0()
+
   return (
     <>
       <Head>
-        <title>Faça Login | voidr</title>
+        <title>Faça Login | Voidr</title>
       </Head>
+
       <BaseLayout currentPage="login">
-        <h1>{t('Vamos começar!')}</h1>
-        <p>{t('Faça o login abaixo')}</p>
-        <button onClick={loginWithRedirect}>Login with redirect</button>
-        <LoginWithGoogle customClass={styles.loginButton} />
+        <Stack
+          padding="10px"
+          width="100vw"
+          height="100vh"
+          direction="row"
+          spacing="10px"
+        >
+          <Stack width="40%">
+            <div className={styles.wrapper}>
+              <Image
+                className={styles.pattern}
+                src={Pattern}
+                alt="Pattern image"
+              />
+
+              <Typography
+                level="h1"
+                fontWeight="sm"
+                fontFamily="Space Grotesk"
+                fontSize="38px"
+              >
+                {t('pages.login.helper.title')}
+              </Typography>
+
+              <Image className={styles.imageLogo} src={Logo} alt="Voidr Logo" />
+            </div>
+          </Stack>
+          <Stack>
+            <Typography>{t('pages.login.form.title')}</Typography>
+            <Button>Teste</Button>
+          </Stack>
+        </Stack>
       </BaseLayout>
     </>
   )
