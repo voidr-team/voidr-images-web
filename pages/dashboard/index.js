0928746ec1bbe738278a741490e7e492360f5a96
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-09-01 16:29:34 -0300

    fix: :lock: fix on the redirect dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/router'
 import Vendors from './vendors'
+import { useEffect } from 'react'
 
 function Dashboard() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.pathname === '/dashboard') {
+      router.push('/dashboard/vendors')
+    }
+  }, [router.pathname])
+
   return <Vendors />
 }
 
