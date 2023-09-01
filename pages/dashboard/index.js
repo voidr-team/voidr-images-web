fb8eecb0b0cb924534f6c087a075abe128b7a12e
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-09-01 16:29:34 -0300

    fix: :lock: fix on the redirect dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,17 @@
-import useAuth from '@/context/auth/useAuth'
+import { useRouter } from 'next/router'
+import Vendors from './vendors'
+import { useEffect } from 'react'
 
-const Dashboard = () => {
-  const { user } = useAuth()
+function Dashboard() {
+  const router = useRouter()
 
-  return <div>{JSON.stringify(user)}</div>
+  useEffect(() => {
+    if (router.pathname === '/dashboard') {
+      router.push('/dashboard/vendors')
+    }
+  }, [router.pathname])
+
+  return <Vendors />
 }
 
 export default Dashboard
