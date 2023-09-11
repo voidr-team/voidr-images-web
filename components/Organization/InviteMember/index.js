b6403ac9e2283f1d4ba2c515818567a3cc3e50ec
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-09-11 17:55:07 -0300

    refactor: change modal

--- components/Organization/InviteMember/index.js
+++ components/Organization/InviteMember/index.js
@@ -2,122 +2,91 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Modal,
-  ModalDialog,
   Stack,
-  Typography,
   Button,
   Autocomplete,
   CircularProgress,
 } from '@mui/joy'
-import Icon from '@/components/UI/Icon'
 import PropTypes from 'prop-types'
 import styles from './InviteMember.module.scss'
 import useInviteMember from './useInviteMember'
 import ErrorMessage from '@/components/UI/ErrorMessage'
 import { Controller } from 'react-hook-form'
+import Modal from '@/components/UI/Modal'
 
 function InviteMember({ isOpen, setIsOpen }) {
   const { onSubmit, formMethods, isLoadingSendInvite, isLoadingRoles, roles } =
     useInviteMember(setIsOpen)
 
   return (
-    <Modal open={isOpen} onClose={() => setIsOpen((prevState) => !prevState)}>
-      <ModalDialog>
-        <Stack>
-          <Stack
-            className={styles.headerWrapper}
-            marginBottom={2}
-            direction="row"
-            justifyContent="space-between"
-            spacing={4}
-          >
-            <Typography level="title-lg">Convidar novo membro</Typography>
-            <button
-              className={styles.buttonCloseModal}
-              onClick={() => {
-                setIsOpen(false)
-              }}
-            >
-              <Icon id="Close_MD" width={25} height={25} />
-            </button>
-          </Stack>
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <div className={styles.inputsWrapper}>
+          <FormControl>
+            <FormLabel>Name</FormLabel>
+            <Input
+              placeholder="John Doe"
+              {...formMethods.register('name', { required: true })}
+            />
+            <ErrorMessage name="name" errors={formMethods.formState.errors} />
+          </FormControl>
 
-          <form className={styles.form} onSubmit={onSubmit}>
-            <div className={styles.inputsWrapper}>
-              <FormControl>
-                <FormLabel>Name</FormLabel>
-                <Input
-                  placeholder="John Doe"
-                  {...formMethods.register('name', { required: true })}
-                />
-                <ErrorMessage
-                  name="name"
-                  errors={formMethods.formState.errors}
-                />
-              </FormControl>
+          <FormControl>
+            <FormLabel>E-mail</FormLabel>
+            <Input
+              placeholder="example@example.com"
+              {...formMethods.register('email', { required: true })}
+            />
+            <ErrorMessage name="email" errors={formMethods.formState.errors} />
+          </FormControl>
+        </div>
 
-              <FormControl>
-                <FormLabel>E-mail</FormLabel>
-                <Input
-                  placeholder="example@example.com"
-                  {...formMethods.register('email', { required: true })}
+        <div className={styles.autocompleteWrapper}>
+          <FormControl>
+            <FormLabel>Cargo</FormLabel>
+            <Controller
+              control={formMethods.control}
+              name={'roles'}
+              render={({ field: { onChange, name, onBlur, value } }) => (
+                <Autocomplete
+                  placeholder="Cargos do membro"
+                  id="user-roles-invite"
+                  multiple
+                  value={value || []}
+                  name={name}
+                  onBlur={onBlur}
+                  onChange={(_event, value) => {
+                    return onChange(value ?? [])
+                  }}
+                  options={roles}
+                  getOptionLabel={(option) => option?.name}
+                  isOptionEqualToValue={(option, value) =>
+                    option?.name === value?.name
+                  }
+                  loading={isLoadingRoles}
+                  endDecorator={
+                    isLoadingRoles ? (
+                      <CircularProgress
+                        size="sm"
+                        sx={{ bgcolor: 'background.surface' }}
+                      />
+                    ) : null
+                  }
+                  sx={() => ({
+                    paddingX: 0.5,
+                  })}
                 />
-                <ErrorMessage
-                  name="email"
-                  errors={formMethods.formState.errors}
-                />
-              </FormControl>
-            </div>
+              )}
+            />
+          </FormControl>
+        </div>
 
-            <div className={styles.autocompleteWrapper}>
-              <FormControl>
-                <FormLabel>Cargo</FormLabel>
-                <Controller
-                  control={formMethods.control}
-                  name={'roles'}
-                  render={({ field: { onChange, name, onBlur, value } }) => (
-                    <Autocomplete
-                      placeholder="Cargos do membro"
-                      id="user-roles-invite"
-                      multiple
-                      value={value || []}
-                      name={name}
-                      onBlur={onBlur}
-                      onChange={(_event, value) => {
-                        return onChange(value ?? [])
-                      }}
-                      options={roles}
-                      getOptionLabel={(option) => option?.name}
-                      isOptionEqualToValue={(option, value) =>
-                        option?.name === value?.name
-                      }
-                      loading={isLoadingRoles}
-                      endDecorator={
-                        isLoadingRoles ? (
-                          <CircularProgress
-                            size="sm"
-                            sx={{ bgcolor: 'background.surface' }}
-                          />
-                        ) : null
-                      }
-                      sx={() => ({
-                        paddingX: 0.5,
-                      })}
-                    />
-                  )}
-                />
-              </FormControl>
-            </div>
-
-            <Stack marginY={2}>
-              <Button loading={isLoadingSendInvite} type="submit">
-                Enviar convite
-              </Button>
-            </Stack>
-          </form>
+        <Stack marginY={2}>
+          <Button loading={isLoadingSendInvite} type="submit">
+            Enviar convite
+          </Button>
         </Stack>
-      </ModalDialog>
+      </form>
     </Modal>
   )
 }
