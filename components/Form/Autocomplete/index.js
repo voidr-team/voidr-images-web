f6e84d98e9fb238c45502c3d83b08a79559c763a
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-09-12 14:40:58 -0300

    style: autocomplete

---
+++ components/Form/Autocomplete/index.js
@@ -0,0 +1,63 @@
+import {
+  FormControl,
+  FormLabel,
+  Autocomplete as AutocompleteProvider,
+  CircularProgress,
+} from '@mui/joy'
+import { useFormContext, Controller } from 'react-hook-form'
+
+function Autocomplete({
+  label,
+  name,
+  options,
+  defaultValue,
+  multiple = false,
+  id,
+  isLoading,
+  placeholder,
+}) {
+  const { control } = useFormContext()
+
+  return (
+    <FormControl>
+      <FormLabel>{label}</FormLabel>
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { onChange, name, onBlur, value } }) => (
+          <AutocompleteProvider
+            placeholder={placeholder}
+            id={id}
+            multiple={multiple}
+            value={value}
+            defaultValue={defaultValue}
+            name={name}
+            onBlur={onBlur}
+            onChange={(_event, value) => {
+              return onChange(value ?? [])
+            }}
+            options={options}
+            getOptionLabel={(option) => option?.name}
+            isOptionEqualToValue={(option, value) =>
+              option?.name === value?.name
+            }
+            loading={isLoading}
+            endDecorator={
+              isLoading ? (
+                <CircularProgress
+                  size="sm"
+                  sx={{ bgcolor: 'background.surface' }}
+                />
+              ) : null
+            }
+            sx={() => ({
+              paddingX: 0.5,
+            })}
+          />
+        )}
+      />
+    </FormControl>
+  )
+}
+
+export default Autocomplete
