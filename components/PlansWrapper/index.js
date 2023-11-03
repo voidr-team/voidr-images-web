d22b1a04cea045d9c90f2758f69f84282fd4367b
buchalla <vs_buchalla@hotmail.com>
2023-11-03 13:20:01 -0300

    fix: small adjustments

--- components/PlansWrapper/index.js
+++ components/PlansWrapper/index.js
@@ -1,12 +1,19 @@
 import { Stack, Typography } from '@mui/joy'
 import PlanInfo from '../PlanInfo'
 import ChoosePlan from './ChoosePlan'
+import { ChevronRight } from 'lucide-react'
 
 export default function PlansWrapper() {
   return (
     <Stack paddingX={3} marginY={4}>
-      <Typography fontWeight="600" level="h4">
-        Plans &gt; Starter
+      <Typography
+        fontWeight="600"
+        level="h4"
+        alignContent="center"
+        alignItems="center"
+        display="flex"
+      >
+        Plans <ChevronRight /> Starter
       </Typography>
 
       <PlanInfo />
