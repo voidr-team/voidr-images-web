e0ed45a95862e6da48df61c6175a84bd2352e975
buchalla <vs_buchalla@hotmail.com>
2023-11-03 22:20:22 -0300

    fix: organization list

--- components/Organization/List/index.js
+++ components/Organization/List/index.js
@@ -1,4 +1,12 @@
-import { Tab, TabList, Tabs, TabPanel, Stack, Typography } from '@mui/joy'
+import {
+  Tab,
+  TabList,
+  Tabs,
+  TabPanel,
+  Stack,
+  Typography,
+  tabClasses,
+} from '@mui/joy'
 import ListMembers from './ListMembers'
 import ListInvites from './ListInvites'
 import Button from '@/components/UI/Button'
@@ -30,9 +38,9 @@ function List({ setIsOpenInviteMember }) {
           sx={(theme) => ({
             gap: 1,
             [`[aria-selected="true"]`]: {
-              bgcolor: theme.vars.palette.primary[500],
               fontWeight: 600,
               color: theme.colorSchemes.light.palette.helper[500],
+              background: 'none !important',
             },
           })}
         >
