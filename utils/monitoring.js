eec16becadef790fda2bf44c8b0caedaa69cdd75
Milson R C Júnior <junior.milson17@gmail.com>
2023-11-09 18:07:55 -0300

    fix: logrocket helper

--- utils/monitoring.js
+++ utils/monitoring.js
@@ -1,6 +1,6 @@
 export default async function startMonitoring({ name, email, id }) {
   if (process.env.NODE_ENV === 'production') {
-    const logRocket = (await import('logrocket')).default
+    const LogRocket = (await import('logrocket')).default
     LogRocket.init('acfgqb/voidr-images-frontend')
     LogRocket.identify(id, {
       name,
