35c563dc021b06841afb7035591645351779e920
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-11-10 14:27:05 -0300

    feat: banner voidr images

--- components/DashboardWrapper/Insights/index.js
+++ components/DashboardWrapper/Insights/index.js
@@ -2,9 +2,11 @@ import { Lightbulb, TrendingUp } from 'lucide-react'
 import styles from './Insights.module.scss'
 import Widget from '@/components/UI/Widget'
 import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
 
 export default function Insights() {
-  const { t } = useTranslation(['translations', 'common'])
+  const { t, i18n } = useTranslation(['translations', 'common'])
+
   return (
     <Widget title={t('insights.title')}>
       <article className={styles.helpBox}>
@@ -22,6 +24,25 @@ export default function Insights() {
 
         <p>{t('insights.content.2')}</p>
       </article>
+
+      {!i18n.language === 'en' ? null : (
+        <a
+          href="https://chat.whatsapp.com/HGk47PecZXRFRgxJ32PKX0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <figure className={styles.bannerImageWrapper}>
+            <Image
+              src="/images/voidr-banner-pt.png"
+              alt="Banner Voidr Enter Comunnity"
+              width={300}
+              height={300}
+              quality={100}
+              priority={true}
+            />
+          </figure>
+        </a>
+      )}
     </Widget>
   )
 }
