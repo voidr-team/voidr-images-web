56f4b8e0c92d4279d663a834bb1a8aa9aecfc114
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-11-13 15:43:05 -0300

    style: add ui with translations to referral page

--- components/ReferralWrapper/index.js
+++ components/ReferralWrapper/index.js
@@ -2,9 +2,13 @@ import Image from 'next/image'
 import styles from './ReferralWrapper.module.scss'
 import { Typography } from '@mui/joy'
 import { useTranslation } from 'next-i18next'
+import CopyURL from '../CopyURL'
+import useAuth from '@/context/auth/useAuth'
+import Icon from '../UI/Icon'
 
 export default function ReferralWrapper() {
   const { t } = useTranslation(['translations', 'common'])
+  const { user } = useAuth()
 
   return (
     <section className={styles.referralWrapper}>
@@ -20,6 +24,23 @@ export default function ReferralWrapper() {
         <Typography level="body-md">
           {t('referral.invite.description')}
         </Typography>
+
+        <div className={styles.copyUrlWrapper}>
+          <Typography level="body-md">
+            {t('referral.invite.copy_url_title')}
+          </Typography>
+          <CopyURL
+            url={`https://app.voidr.co/referral/${user?.currentProject?.name}`}
+          />
+        </div>
+
+        <div className={styles.shareLink}>
+          <Typography level="body-md">
+            {t('referral.invite.share_title')}
+          </Typography>
+          <Icon id="Whatsapp_Icon_Outlined" width={40} height={40} />
+          <Icon id="Telegram_Icon_Outlined" width={40} height={40} />
+        </div>
       </div>
     </section>
   )
