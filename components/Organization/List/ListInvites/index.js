4cfff271e82eacbd71ca76013da91109760e5b18
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-11-17 16:10:10 -0300

    hotfix: add function to invite url

--- components/Organization/List/ListInvites/index.js
+++ components/Organization/List/ListInvites/index.js
@@ -44,55 +44,57 @@ function ListInvites() {
               </tr>
             </thead>
             <tbody>
-              {data?.data.map((invite) => (
-                <tr key={invite.id}>
-                  <td>{invite.invitee.email}</td>
-                  <td>{t('common:pending')}</td>
-                  <td>{formatDateLocal(invite.created_at)}</td>
-                  <td>{formatDateLocal(invite.expires_at)}</td>
-                  <td>{invite.inviter.name}</td>
-                  <td>
-                    <Dropdown>
-                      <MenuButton sx={{ padding: 1 }}>
-                        <Icon height={20} width={20} id="More_Horizontal" />
-                      </MenuButton>
-                      <Menu
-                        sx={(theme) => ({
-                          backgroundColor: theme.vars.palette.primary[500],
-                        })}
-                      >
-                        <Stack padding="5px">
-                          <CopyClipboard text={invite.invitation_url}>
-                            <MenuItem>
-                              <Typography fontSize={14} fontWeight="600">
-                                {t('inviations.table.copy_invite')}
+              {data?.data.map((invite) => {
+                return (
+                  <tr key={invite.id}>
+                    <td>{invite.invitee.email}</td>
+                    <td>{t('common:pending')}</td>
+                    <td>{formatDateLocal(invite.created_at)}</td>
+                    <td>{formatDateLocal(invite.expires_at)}</td>
+                    <td>{invite.inviter.name}</td>
+                    <td>
+                      <Dropdown>
+                        <MenuButton sx={{ padding: 1 }}>
+                          <Icon height={20} width={20} id="More_Horizontal" />
+                        </MenuButton>
+                        <Menu
+                          sx={(theme) => ({
+                            backgroundColor: theme.vars.palette.primary[500],
+                          })}
+                        >
+                          <Stack padding="5px">
+                            <CopyClipboard text={invite?.invitationUrl}>
+                              <MenuItem>
+                                <Typography fontSize={14} fontWeight="600">
+                                  {t('inviations.table.copy_invite')}
+                                </Typography>
+                              </MenuItem>
+                            </CopyClipboard>
+
+                            <ListDivider />
+
+                            <MenuItem onClick={() => cancelInvite(invite.id)}>
+                              <Icon
+                                height={20}
+                                width={20}
+                                id="Remove_Minus_Circle"
+                              />
+                              <Typography
+                                fontSize={14}
+                                fontWeight="500"
+                                textColor="danger.700"
+                                paddingLeft={0.5}
+                              >
+                                {t('inviations.table.remove_invite')}
                               </Typography>
                             </MenuItem>
-                          </CopyClipboard>
-
-                          <ListDivider />
-
-                          <MenuItem onClick={() => cancelInvite(invite.id)}>
-                            <Icon
-                              height={20}
-                              width={20}
-                              id="Remove_Minus_Circle"
-                            />
-                            <Typography
-                              fontSize={14}
-                              fontWeight="500"
-                              textColor="danger.700"
-                              paddingLeft={0.5}
-                            >
-                              {t('inviations.table.remove_invite')}
-                            </Typography>
-                          </MenuItem>
-                        </Stack>
-                      </Menu>
-                    </Dropdown>
-                  </td>
-                </tr>
-              ))}
+                          </Stack>
+                        </Menu>
+                      </Dropdown>
+                    </td>
+                  </tr>
+                )
+              })}
             </tbody>
           </table>
         </section>
