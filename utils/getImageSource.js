01508cacb135880999f568f3f7995b22b18bbdfc
buchalla <vs_buchalla@hotmail.com>
2023-11-28 15:12:13 -0300

    fix: when image src is empty

--- utils/getImageSource.js
+++ utils/getImageSource.js
@@ -1,5 +1,8 @@
 const VOIDR_URL_IMAGE = process.env.NEXT_PUBLIC_VOIDR_URL_IMAGE
 
 export default function getImageSource(imageUrl = '') {
+  if (!imageUrl) {
+    return '#'
+  }
   return `${VOIDR_URL_IMAGE}${imageUrl.replace('v1/images/', '')}`
 }
